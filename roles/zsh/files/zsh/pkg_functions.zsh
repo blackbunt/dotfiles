0d3a6c0c24c4492c3384cdfb136dfbd92cdf8f20
blackbunt <blackbunt@posteo.se>
2025-03-09 19:38:13 +0100

    added zsh

--- roles/zsh/files/zsh/pkg_functions.zsh
+++ roles/zsh/files/zsh/pkg_functions.zsh
@@ -6,54 +6,6 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
-k9s-upgrade() {
-    VERSION=$(get_latest_release derailed/k9s)
-    pushd /tmp > /dev/null 2>&1
-    # Download the binary
-    echo -e "${ARROW} ${GREEN}Downloading k9s ${VERSION}${NC}"
-    wget -q https://github.com/derailed/k9s/releases/download/$VERSION/k9s_Linux_x86_64.tar.gz
-    # Extract the binary
-    echo -e "${ARROW} ${GREEN}Extracting k9s ${VERSION}${NC}"
-    tar -xzf k9s_Linux_x86_64.tar.gz
-    # Move the binary to /usr/local/bin
-    sudo mv k9s /usr/local/bin
-    # Remove the tar file
-    rm k9s_Linux_x86_64.tar.gz
-    echo -e "${ARROW} ${GREEN}k9s ${VERSION} installed${NC}"
-    k9s version
-    popd > /dev/null 2>&1
-}
-
-gone-upgrade() {
-    VERSION=$(get_latest_release guillaumebreton/gone)
-    pushd /tmp > /dev/null 2>&1
-    # Download the binary
-    echo -e "${ARROW} ${GREEN}Downloading gone ${VERSION}${NC}"
-    wget -q https://github.com/guillaumebreton/gone/releases/download/$VERSION/gone_Linux_x86_64.tar.gz
-    echo -e "${ARROW} ${GREEN}Extracting gone ${VERSION}${NC}"
-    tar -xzf gone_Linux_x86_64.tar.gz
-    sudo mv gone /usr/local/bin
-    rm gone_Linux_x86_64.tar.gz
-    echo -e "${ARROW} ${GREEN}gone ${VERSION} installed${NC}"
-    popd > /dev/null 2>&1
-}
-
-pinger-upgrade() {
-    VERSION=$(get_latest_release hirose31/pinger)
-    pushd /tmp > /dev/null 2>&1
-    # Download the binary
-    echo -e "${ARROW} ${GREEN}Downloading pinger ${VERSION}${NC}"
-    wget -q https://github.com/hirose31/pinger/releases/download/${VERSION}/pinger_${VERSION}_linux_amd64.tar.gz
-    echo -e "${ARROW} ${GREEN}Extracting pinger ${VERSION}${NC}"
-    tar -xzf pinger_${VERSION}_linux_amd64.tar.gz
-    cd pinger_${VERSION}_linux_amd64
-    sudo mv pinger /usr/local/bin
-    cd ..
-    rm pinger_${VERSION}_linux_amd64.tar.gz -rf
-    echo -e "${ARROW} ${GREEN}pinger ${VERSION} installed${NC}"
-    popd > /dev/null 2>&1
-}
-
 go-upgrade() {
     # if no arg is passed, get latest version
     if [[ -z $1 ]]; then
