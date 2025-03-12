d75d5b7c4bcc7bd60f5458796e2cd16e22c23ac8
blackbunt <blackbunt@posteo.se>
2025-03-12 22:06:36 +0100

    added some lsd aliases

--- roles/zsh/files/zsh/lsd_aliases.zsh
+++ roles/zsh/files/zsh/lsd_aliases.zsh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
 alias ll='lsd -hAl --group-directories-first'
+alias l='lsd -l'
+alias la='lsd --tree'
+alias lsf='lsd -l | grep -v "/$"'
+alias lsh='lsd -a'
+alias lls='lsd -lh'
+alias lt='lsd --tree -h'
